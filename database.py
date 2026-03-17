ece66942dc8ec5a1c792381e7c0a24634aaa5a08
Nabea-ken <k.mugambi@ymail.com>
2026-03-17 15:52:43 -0400

    Register and login authenticated Users.

--- database.py
+++ database.py
@@ -3,7 +3,8 @@
 #Define the table structure using classes and objects.
 
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import String, Integer
+from sqlalchemy import String, Integer, DateTime
+from datetime import datetime
 
 class Base(DeclarativeBase):
     pass
@@ -14,3 +15,13 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(100))
     location: Mapped[str] = mapped_column(String(100))
+    age: Mapped[int] = mapped_column(Integer)
+
+class Authentication(Base):
+    __tablename__ = "user_authentication"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    full_name: Mapped[str] = mapped_column(String(100))
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    password: Mapped[str] = mapped_column(String(100))
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+
--- main.py
+++ main.py
@@ -8,7 +8,7 @@
 from flask import Flask, request, jsonify
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker
-from database import Base, User
+from database import Base, User, Authentication
 from flask_cors import CORS
 
 
@@ -33,8 +33,10 @@ my_session = session()
 # create tables automatically based on the defined classes in database.py
 Base.metadata.create_all(engine)
 
+# Drop all tables
 
 
+# Casa
 @app.route("/", methods=allowed_methods)
 def home():
     if request.method.upper() == "GET":
@@ -44,7 +46,7 @@ def home():
         return jsonify({"error": "Method not allowed"}), 405
     
     
-
+# A route to handle GET and POST requests for users.
 @app.route("/users", methods=allowed_methods)
 def users():
     if request.method.upper() == 'GET':
@@ -56,7 +58,8 @@ def users():
             data.append({
                 "id": user.id,
                 "name": user.name,
-                "location": user.location
+                "location": user.location,
+                "age": user.age
             })
         return jsonify({"data": data}), 200
     
@@ -64,17 +67,92 @@ def users():
         #Convert JSON to Dictionary
         data = request.get_json()
         #Check if all fields are received
-        if data["name"] == "" or data["location"] == "":
-            return jsonify({"error": "Name and Location cannot be empty!"}), 400
+        if data["name"] == "" or data["location"] == "" or data["age"] == "":
+            return jsonify({"error": "Name, Location, and Age cannot be empty!"}), 400
         else:
             #Store user in users table using SQLAlchemy
-            new_user = User(name=data["name"], location=data["location"])
+            new_user = User(name=data["name"], location=data["location"], age=data["age"])
             my_session.add(new_user)
             my_session.commit()
             return jsonify({"message": f"User created successfully! {data['name']} from {data['location']}"}), 201
         
     else:
         return jsonify({"error" : "Method Not Allowed!"}), 405
+    
+
+# Register route to create a new user in the user_authentication table with full_name, email, and password fields. 
+# Check if email already exists before creating a new user. Return appropriate status codes and messages for success and error cases.
+@app.route("/register", methods=allowed_methods)
+def register():
+    try:
+        data = request.get_json()
+
+        #debug
+        print("Incoming:", data)  
+
+        if not data:
+            return jsonify({"error": "No data received"}), 400
+
+        full_name = data.get("full_name")
+        email = data.get("email")
+        password = data.get("password")
+
+        if not full_name or not email or not password:
+            return jsonify({"error": "Full Name, Email, and Password cannot be empty!"}), 400
+        
+
+        existing_user = my_session.query(Authentication).filter_by(email=data["email"]).first()
+
+        if existing_user:
+            return jsonify({"error": "Email already registered"}), 409
+
+        new_user = Authentication(
+            full_name=full_name,
+            email=email,
+            password=password
+        )
+
+        my_session.add(new_user)
+        my_session.commit()
+
+        return jsonify({"message": f"User created successfully! {full_name}"}), 201
+
+        
+
+    except Exception as e:
+        print("ERROR:", str(e)) 
+        return jsonify({"error": "Server error"}), 500
+
+
+# Login route to authenticate users based on email and password stored in the user_authentication table
+@app.route("/login", methods=allowed_methods)
+def login():
+    try:
+        data = request.get_json()
+
+        if not data:
+            return jsonify({"error": "No data received"}), 400
+
+        email = data.get("email")
+        password = data.get("password")
+
+        if not email or not password:
+            return jsonify({"error": "Email and Password cannot be empty!"}), 400
+
+        query = select(Authentication).where(Authentication.email == email)
+        user = my_session.scalars(query).first()
+
+        if user is None:
+            return jsonify({"error": "User not found!"}), 404
+
+        if user.password == password:
+            return jsonify({"message": f"Login successful! Welcome {user.full_name}"}), 200
+        else:
+            return jsonify({"error": "Incorrect password!"}), 401
+
+    except Exception as e:
+        print("ERROR:", str(e))
+        return jsonify({"error": "Server error"}), 500
     
 
 """ @app.route("/register", methods=allowed_methods)
