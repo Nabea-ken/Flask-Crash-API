41ac9286a09c5552f161e43deacd02f215f61f46
Nabea-ken <k.mugambi@ymail.com>
2026-03-24 12:49:20 -0400

    Modify register route with if 'post' and clean the code.

--- main.py
+++ main.py
@@ -14,10 +14,8 @@ from sqlalchemy.orm import sessionmaker
 from database import Base, Employee, Authentication
 from flask_cors import CORS
 
-
 app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = "ajkgaijeroannnv"
-
 
 CORS(app)
 
@@ -32,26 +30,21 @@ jwt = JWTManager(app)
 
 bcrypt = Bcrypt(app)
 
-
 allowed_methods = ["GET", "POST", "PUT", "DELETE", "PATCH"]
-
 
 DATABASE_URL = "postgresql+psycopg2://postgres:postgres@localhost:5433/flask_crash_api"
 
 # Connecting SQLAlchemy to PostgreSQL database using create_engine
 engine = create_engine(DATABASE_URL, echo=True)
 
-
 # Create a session to call query methods
 session = sessionmaker(bind=engine)
 my_session = session()
-
 
 # create tables automatically based on the defined classes in database.py
 Base.metadata.create_all(engine)
 
 # Drop all tables
-
 
 # Casa
 @app.route("/", methods=allowed_methods)
@@ -61,8 +54,7 @@ def home():
         return jsonify(msg), 200
     else:
         return jsonify({"error": "Method not allowed"}), 405
-    
-    
+      
 # A route to handle GET and POST requests for employees.
 @app.route("/employees", methods=allowed_methods)
 @jwt_required()
@@ -100,55 +92,51 @@ def employees():
     else:
         return jsonify({"error" : "Method Not Allowed!"}), 405
     
-
 # Register route to create a new user in the user_authentication table with full_name, email, and password fields. 
 # Check if email already exists before creating a new user. Return appropriate status codes and messages for success and error cases.
 @app.route("/register", methods=allowed_methods)
 def register():
-    try:
-        data = request.get_json()
+    if request.method.upper() == 'POST':
+        try:
+            data = request.get_json()
 
-        #debug
-        print("Incoming:", data)  
+            #debug
+            print("Incoming:", data)  
 
-        if not data:
-            return jsonify({"error": "No data received"}), 400
+            if not data:
+                return jsonify({"error": "No data received"}), 400
 
-        full_name = data.get("full_name")
-        email = data.get("email")
-        password = data.get("password")
+            full_name = data.get("full_name")
+            email = data.get("email")
+            password = data.get("password")
 
-        if not full_name or not email or not password:
-            return jsonify({"error": "Full Name, Email, and Password cannot be empty!"}), 400
-        
+            if not full_name or not email or not password:
+                return jsonify({"error": "Full Name, Email, and Password cannot be empty!"}), 400
+            
+            existing_user = my_session.query(Authentication).filter_by(email=data["email"]).first()
 
-        existing_user = my_session.query(Authentication).filter_by(email=data["email"]).first()
+            if existing_user:
+                return jsonify({"error": "Email already registered"}), 409
 
-        if existing_user:
-            return jsonify({"error": "Email already registered"}), 409
+            hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
 
-        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+            new_user = Authentication(
+                full_name=full_name,
+                email=email,
+                password=hashed_password
+            )
 
-        new_user = Authentication(
-            full_name=full_name,
-            email=email,
-            password=hashed_password
-        )
+            my_session.add(new_user)
+            my_session.commit()
 
-        my_session.add(new_user)
-        my_session.commit()
+            # Generate a unique token using the user email
+            token = create_access_token(identity=data["email"])
 
-        # Generate a unique token using the user email
-        token = create_access_token(identity=data["email"])
+            return jsonify({"message": f"User created successfully! {full_name}", "token": token}), 201
 
-        return jsonify({"message": f"User created successfully! {full_name}", "token": token}), 201
-
-        
-
-    except Exception as e:
-        print("ERROR:", str(e)) 
-        return jsonify({"error": "Server error"}), 500
-
+        except Exception as e:
+            print("ERROR:", str(e)) 
+            return jsonify({"error": "Server error"}), 500
 
 # Login route to authenticate users based on email and password stored in the user_authentication table
 @app.route("/login", methods=allowed_methods)
@@ -187,25 +175,6 @@ def login():
         print("ERROR:", str(e))
         return jsonify({"error": "Server error"}), 500
    
-
-
-""" @app.route("/register", methods=allowed_methods)
-def register():
-    data = request.get_json
-
-    if data["username"] == "" or data["email"] == "" or data["password"]:
-            return jsonify({"error": "Fields cannot be empty!"}), 400
-        else:
-            #Store user in users table using SQLAlchemy
-            new_user = User(username=data["username"], email=data["email"], password=data["password"])
-            my_session.add(new_user)
-            my_session.commit()
-            return jsonify({"message": f"User created successfully! {data['username']} "}), 201 """
-
-
-
-
-
 
 
 app.run(debug=True)
